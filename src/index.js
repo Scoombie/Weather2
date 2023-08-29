f29ddaee5226b8dc5862b6b7ff87bef198af75c7
Scoombie <ulmpopi194@gmail.com>
2023-08-29 16:44:54 +0200

    updated JS

---
+++ src/index.js
@@ -0,0 +1,64 @@
+/**function displayTemperature(response) {
+  let temperatureElement = document.querySelector("temperature");
+  temperatureElement.innerHTML = math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.main.temp;
+}
+
+let apiKey = "f14b4bb2b4beec5fc26aebota3044f2e";
+let city = "New york";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New york&key=f14b4bb2b4beec5fc26aebota3044f2e&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);*/
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.icon);
+}
+
+let apiKey = "f14b4bb2b4beec5fc26aebota3044f2e";
+let city = "New york";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New york&key=f14b4bb2b4beec5fc26aebota3044f2e&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
