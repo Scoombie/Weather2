4e7037d31ade244f95ee2a04e48032d4bf1e0b0e
Scoombie <ulmpopi194@gmail.com>
2023-08-30 12:15:45 +0200

    Imported axios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-/**function displayTemperature(response) {
-  let temperatureElement = document.querySelector("temperature");
-  temperatureElement.innerHTML = math.round(response.data.main.temp);
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.main.temp;
-}
-
-let apiKey = "f14b4bb2b4beec5fc26aebota3044f2e";
-let city = "New york";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New york&key=f14b4bb2b4beec5fc26aebota3044f2e&units=metric`;
-
-console.log(apiUrl);
-
-axios.get(apiUrl).then(displayTemperature);*/
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -39,14 +25,14 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
+  let humidityElement = document.querySelector("#Humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML = response.data.weather[0].description;
+  descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
